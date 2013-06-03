6c836a65dfa91b4ab72b920d25f85be23c63d508
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-06-03 10:43:07 -0400

    update gbifmap2

--- gbifmap2/code.R
+++ gbifmap2/code.R
@@ -1,5 +1,4 @@
 require(rgbif)
-out <- occurrencelist(scientificname = 'Puma concolor', coordinatestatus = TRUE, maxresults = 100)
 toGeoJSON = function(list_, lat = 'latitude', lon = 'longitude'){
   x = lapply(list_, function(l){
     if (is.null(l[[lat]]) || is.null(l[[lon]])){
@@ -25,8 +24,8 @@ gbifmap2 <- function(input = NULL, map_provider = 'MapQuestOpen.OSM', map_zoom =
   L1$set(height = 800, width = 1600)
   L1$setView(c(30, -73.90), map_zoom)
   L1$geoJson(toGeoJSON(input, lat = 'decimalLatitude', lon = 'decimalLongitude'), 
-             onEachFeature = '#! function(feature, layer){
-      layer.bindPopup(feature.properties.taxonName)
+    onEachFeature = '#! function(feature, layer){
+      layer.bindPopup(feature.properties.popup || feature.properties.taxonName)
     } !#',
              pointToLayer =  "#! function(feature, latlng){
        return L.circleMarker(latlng, {
@@ -58,13 +57,28 @@ map1$save('gbifmap2/index.html', cdn = TRUE)
 
 ## Example 2
 splist <- c('Accipiter erythronemius', 'Junco hyemalis', 'Aix sponsa')
-out2 <- occurrencelist_many(splist, coordinatestatus = TRUE, maxresults = 20)
+out2 <- occurrencelist_many(splist, coordinatestatus = TRUE, 
+  maxresults = 20, format="darwin") 
+out2 <- out2[,c("taxonName","county","decimalLatitude","decimalLongitude",
+    "institutionCode","collectionCode","catalogNumber","basisOfRecordString","collector")] 
+out2$taxonName <- capwords(out2$taxonName, onlyfirst=TRUE)
+
+
+
 mycolors = c('red', 'blue', 'green')
 out2 <- mutate(out2, 
   taxonName = as.factor(taxonName),
   fillColor = mycolors[as.numeric(taxonName)]
 )
+
 out_list2 <- apply(out2, 1, as.list) 
+
+# add popup
+out_list2 <- lapply(out_list2, function(l){
+  l$popup = paste(paste("<b>", names(l), ": </b>", l, "<br/>"), collapse = '\n')
+  return(l)
+})
+
 map2 = gbifmap2(out_list2)
 map2$save('gbifmap2/example2.html', cdn = TRUE)
 
